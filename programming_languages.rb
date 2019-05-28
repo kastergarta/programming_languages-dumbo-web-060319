3c6b973fcff782c2e963847a6f0f327457de8caa
Dennis <dennis.basyrov@gmail.com>
2019-05-28 03:53:01 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -34,9 +34,9 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |type, hash|
     hash.each do |name, attributes|
-      new_languages[name] ||= attributes
-      new_languages[name][:style] ||= []
-      new_languages[name][:style] << type
+      new_hash[name] ||= attributes
+      new_hash[name][:style] ||= []
+      new_hash[name][:style] << type
     end
   end
   return new_hash
